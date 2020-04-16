4b2e54025cda68cf7caa59412197d1fab1b94b40
Jack Antcio <anticoj@bc.edu>
2020-04-16 15:26:46 -0400

    Added social icons

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './Contact.css';
+import { SocialIcon } from 'react-social-icons';
 
 class Contact extends Component {
 
   render() {
     return (
       <div className="contact" id="contact">
+        <SocialIcon url="mailto:anticoj@bc.edu " style={{ height: 100, width: 100 }}/>
+        <div className="spacer"></div>
+        <SocialIcon url="https://www.instagram.com/carsagainstcoloncancer/" style={{ height: 100, width: 100 }}/>
+        <div className="spacer"></div>
+        <SocialIcon url="https://www.youtube.com/watch?v=KLEp6PIhcFM" style={{ height: 100, width: 100 }}/>
+        
       </div>
     );
   }
